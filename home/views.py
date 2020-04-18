073b96a7411f717fb176aee466b4f070e9e77f0f
ashu <ashwinsariwal007@gmail.com>
2020-04-18 19:45:38 +0530

    ui

--- home/views.py
+++ home/views.py
@@ -12,7 +12,14 @@ def homeView(request):
 
     jsonUrl2 = 'https://api.covid19india.org/v2/state_district_wise.json'           # url for districtWise data
     districtData = json.load(urlopen(jsonUrl2))
+    stateOrderWise = []
 
+    def myFunc(e):                                                                  # func to sort state cases in
+        return int(e['confirmed'])                                                  # decreasing order
+    stateData["statewise"].sort(reverse=True, key=myFunc)
+
+    for oneData in districtData:
+        oneData["districtData"].sort(reverse=True, key=myFunc)
     data_for_frontend = {
         'dataStateWise': stateData["statewise"],
         'dataDistrictWise': districtData,
